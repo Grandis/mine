49729f081d39d969de7eb9c13b37a571fdfaeebd
Grandis <grandis.kepr@gmail.com>
2013-04-29 14:09:36 +0300

    Добавлена сетка на графике.

--- mine/2D.cs
+++ mine/2D.cs
@@ -21,16 +21,43 @@ namespace mine
         {
             Graphics gr = e.Graphics;
             Pen axis = new Pen(Color.Black, 3);
-            Point yMax = new Point(30, 30);
-            Point xMax = new Point(ClientSize.Width - 30, ClientSize.Height - 30);
-            Point nullPoint = new Point(30, ClientSize.Height - 30);
+            int margin = 30; // Отступ графика от краев формы.
+            Point yMax = new Point(margin, margin);
+            Point xMax = new Point(ClientSize.Width - margin, ClientSize.Height - margin);
+            Point nullPoint = new Point(margin, ClientSize.Height - margin);
 
-            // Рисуем оси координат и подписи к ним
+            // Рисуем оси координат и подписи к ним:
             gr.DrawLine(axis, yMax, nullPoint);
             gr.DrawString("Y", new Font(SystemFonts.DefaultFont, FontStyle.Bold), new SolidBrush(Color.Black), 10, 15); 
             gr.DrawLine(axis, nullPoint, xMax);
             gr.DrawString("X", new Font(SystemFonts.DefaultFont, FontStyle.Bold), new SolidBrush(Color.Black), ClientSize.Width - 20, ClientSize.Height - 25); 
             // ------------------------------------
+
+            drawLines(gr, margin); // Рисуем сетку.
         }
+
+        // Сетка на графике:
+        public void drawLines(Graphics gr, int margin)
+        {
+            int segmentsAmount = 5;
+            Pen grid = new Pen(Color.Gray, 1);
+            float width = ClientSize.Width - margin * 2;
+            float height = ClientSize.Height - margin * 2;
+
+            float startHor = margin, startVert = margin;
+            for (int i = 0; i < segmentsAmount; i++)
+            {
+                startHor += width / segmentsAmount;
+                gr.DrawLine(grid, startHor, startVert, startHor, startVert + height);
+            }
+
+            startHor = margin;
+            for (int i = 0; i < segmentsAmount; i++)
+            {
+                gr.DrawLine(grid, startHor, startVert, startHor + width, startVert);
+                startVert += height / segmentsAmount;
+            }
+        }
+        // ------------------------------------------
     }
 }
